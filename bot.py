41b22bb96bb945b0c4824900ff6ea30ebd5984f9
skybro2004 <skybro2004@gmail.com>
2022-02-27 20:28:21 +0900

    급식 명령어 추가

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
-import datetime, os, sqlite3, json
+from cgitb import text
+import datetime, os, sqlite3, json, asyncio
+from tkinter import N
 
 import discord
 
@@ -13,6 +15,7 @@ path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
 
 #모듈 불러오기
 from modules import hangang
+from modules import meal
 
 con = sqlite3.connect(f"{path}/data/database.db")
 cursor = con.cursor()
@@ -20,7 +23,6 @@ cursor = con.cursor()
 keys = {}
 try:
     with open(path + "/key.json", "r") as jsonKeys:
-        print(path + "/key.json")
         keys = json.load(jsonKeys)
 except FileNotFoundError:
     discordToken = input("디스코드 봇의 토큰을 입력하세요 : ")
@@ -30,7 +32,8 @@ except FileNotFoundError:
     with open(path + "/key.json", "w") as jsonKeys:
         json.dump(keys, jsonKeys)
 
-bot = discord.Bot()
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
 
 
 #==================================================
@@ -42,6 +45,7 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.streaming, activity=activity)
     print(f"login as {bot.user}")
     print("="*50)
+    await bot.register_commands(commands=None, guild_id=803249696638238750, force=True)
 
 
 #==================================================
@@ -55,7 +59,7 @@ async def ping(ctx):
 #/status
 @bot.slash_command(guild_ids = [803249696638238750], description="현재 봇 상태 출력")
 async def status(ctx):
-    status_embed = discord.Embed(title="status", description=f"log in as {bot.user}")
+    status_embed = discord.Embed(title="status", description=f"log in as {bot.user}", color=0xe74c3c)
     status_embed.add_field(name="ping", value=f'{round(round(bot.latency, 4)*1000)}ms')
     status_embed.add_field(name="Uptime", value=f"{str(datetime.datetime.now() - startTime).split('.')[0]}", inline=False)
     status_embed.add_field(name="last update", value=lastUpdateTime)
@@ -66,7 +70,7 @@ async def status(ctx):
 @bot.slash_command(name="help" , guild_ids = [803249696638238750], description="도움말을 불러옵니다.")
 @bot.slash_command(name="도움말" , guild_ids = [803249696638238750], description="도움말을 불러옵니다.")
 async def help(ctx):
-    help_embed = discord.Embed(title='도움말')
+    help_embed = discord.Embed(title='도움말', color=0xe74c3c)
     help_embed.add_field(name="/status", value="현재 조교봇의 상태를 불러옵니다.", inline=False)
     help_embed.add_field(name="/한강", value="한강 수온을 불러옵니다.", inline=False)
     help_embed.add_field(name="/급식", value="준비중", inline=False)
@@ -75,11 +79,108 @@ async def help(ctx):
 
 #/급식
 @bot.slash_command(name="급식", guild_ids = [803249696638238750], description="급식을 불러옵니다")
-async def meal(ctx):
-    bot.wait_for
+async def meals(ctx):
+    author = ctx.author.id
+
+    cursor.execute("SELECT id FROM userData")
+    userIdList = []
+    for item in cursor.fetchall():
+        userIdList.append(item[0])
+    
+    if author in userIdList:
+        cursor.execute(f"SELECT * FROM userData WHERE id={author}")
+        userData = cursor.fetchone()
+        todayMeal = meal.getMeal(userData[1], userData[2])
+        if todayMeal["code"]==200:
+            mealEmbed = discord.Embed(title=f"{datetime.datetime.now().strftime('%m월 %d일')} 급식", color=0xe74c3c)
+            for item in todayMeal["data"]:
+                mealEmbed.add_field(name=item["name"], value=", ".join(item["allergy"]), inline=False)
+            mealEmbed.set_footer(text=f"{todayMeal['cal']}")
+
+            await ctx.respond(embed=mealEmbed)
+        elif todayMeal["code"]==416:
+            await ctx.respond("오늘은 급식이 없습니다.")
+        else:
+            await ctx.respond(f"알 수 없는 에러가 발생했습니다.\nError no.{todayMeal['code']}")
+
+        await ctx.respond(embed=mealEmbed)
+
+    else:
+        botMsg = await ctx.respond("학교 이름을 입력해주세요")
+
+        def check(m):
+            return m.author.id == ctx.author.id and m.channel.id == botMsg.channel.id
+
+        while 1:
+            try:
+                userMsg = await bot.wait_for("message", timeout=30.0, check=check)
+            except asyncio.TimeoutError:
+                await botMsg.edit_original_message(content="타임아웃")
+                return
+            else:
+                schlName = userMsg.content
+                await userMsg.delete()
+
+                schlData = meal.getSchl(schlName)
+
+                if schlData["code"]==200:
+                    break
+
+                elif schlData["code"]==416:
+                    await botMsg.edit_original_message(content="학교가 검색되지 않았어요.\n다시 입력해주세요")
+
+                else:
+                    await botMsg.edit_original_message(content=f"알 수 없는 에러가 발생했어요.\nError no.{schlData['code']}")
+                    return
+
+        schlData = schlData["schools"]
+        schools = []
+        for school in schlData:
+            schools.append(discord.SelectOption(label=school["schlName"], description=school["office"], value=json.dumps({"schlCode":school["schlCode"], "officeCode":school["officeCode"]})))
+
+        global selection
+        selection = ""
+
+        class selectSchl(discord.ui.View):
+            @discord.ui.select(placeholder="학교를 선택하세요", options=schools)
+            async def select(self, select: discord.ui.Select, interaction: discord.Interaction):
+                if author==interaction.user.id:
+                    global selection
+                    selection = json.loads(interaction.data["values"][0])
+
+
+            @discord.ui.button(label="확인")
+            async def accept(self, button: discord.ui.Button, interaction: discord.Interaction):
+                if author==interaction.user.id:
+                    global selection
+                    if selection=="":
+                        await ctx.respond("학교를 선택해주세요!", ephemeral=True)
+                    else:
+                        try:
+                            #DB에 저장
+                            cursor.execute(
+                                "INSERT INTO userData(id, officeCode, schlCode) VALUES(?, ?, ?)",
+                                (author, selection["officeCode"], selection["schlCode"])
+                            )
+
+                        except sqlite3.IntegrityError: #이미 등록한 사용자의 경우
+                            #정보업데이트
+                            cursor.execute(
+                                "UPDATE userData SET officeCode=?, schlCode=? WHERE id=?",
+                                (selection["officeCode"], selection["schlCode"], author)
+                            )
+
+                        con.commit()
+
+                        await botMsg.edit_original_message(content="설정 완료!", view=None, delete_after=5.0)
+
+        await botMsg.edit_original_message(content=f"\"{schlName}\" 검색결과", view=selectSchl())
+
+
+
 
 #/시간표
-@bot.slash_command(name="시간표", guild_ids = [803249696638238750], description="급식을 불러옵니다")
+@bot.slash_command(name="시간표", guild_ids = [803249696638238750], description="시간표 불러옵니다")
 async def schedular(ctx):
     pass
 
@@ -90,14 +191,35 @@ async def getHangang(ctx):
     await respondMsg.edit_original_message(content = f"현재 한강 수온 : {hangang.getTemp()}°C")
 
 #/dev
-@bot.slash_command(name="dev", guild_ids = [803249696638238750], description="dev")
+@bot.slash_command(name="dev1", guild_ids = [803249696638238750], description="dev1")
 @discord.has_role(946797378780950608)
-async def dev(ctx):
-    await ctx.respond(f"dev")
+async def dev1(ctx):
+    class dropdown(discord.ui.View):
+        @discord.ui.select(placeholder="asdf", options=[discord.SelectOption(label="test", value="value"), discord.SelectOption(label="test2", value="value2", description="des")])
+        async def textfield(self, select: discord.ui.Select, interaction: discord.Interaction):
+            print(interaction.data)
+
+        @discord.ui.button(label="asdf", style=discord.ButtonStyle.primary)
+        async def primary(self, button: discord.ui.Button, interaction: discord.Integration):
+            await ctx.respond("buttonpress")
+
+    await ctx.respond("dropdown", view=dropdown())
+
+@bot.slash_command(name="dev2", guild_ids = [803249696638238750], description="dev2")
+@discord.has_role(946797378780950608)
+async def dev2(ctx):
+    
+
+    await ctx.respond("dropdown")
 
 #/owner
 @bot.slash_command(name="owner", guild_ids = [803249696638238750], description="owner")
 @discord.is_owner(803249696638238750)
+async def hidden(ctx):
+    print(bot.guilds)
+    await ctx.respond(f"owner")
+
+@bot.slash_command(name="test", guild_ids = [803249696638238750], description="test")
 async def hidden(ctx):
     await ctx.respond(f"owner")
 
--- modules/meal.py
+++ modules/meal.py
@@ -2,8 +2,43 @@ import json, os, datetime
 import urllib.request as ul
 import urllib.parse as parse
 
-def getSchl():
-    pass
+key = "028278aaacd242438668d46a5464e934"
+
+def getSchl(schlName):
+    #url 정하기
+    url = f"https://open.neis.go.kr/hub/schoolInfo?KEY={key}&Type=json&pSize=25"
+    url += f"&SCHUL_NM={parse.quote(schlName)}"
+    #요청
+    request = ul.Request(url)
+    #응답
+    response = ul.urlopen(request)
+
+    #정상적으로 불러옴
+    if response.getcode()==200:
+        #응답 데이터 읽기
+        responseData = response.read()
+        #json으로 디코드
+        responseData = json.loads(responseData)
+
+        try:
+            schlCount = responseData["schoolInfo"][0]["head"][0]["list_total_count"]
+            #데이터 정제
+            responseData = responseData["schoolInfo"][1]["row"]
+            schools = []
+            #필요한 데이터(학교 이름, 도,시 이름, 학교 코드, 도,시 코드)만 뽑아와서 school 배열 안에 정리
+            for i in range(schlCount):
+                schools.append({"schlName":responseData[i]["SCHUL_NM"], "schlCode":responseData[i]["SD_SCHUL_CODE"], "office":responseData[i]["LCTN_SC_NM"], "officeCode":responseData[i]["ATPT_OFCDC_SC_CODE"]})
+            #리턴
+            return {"code":200, "schlCount":schlCount, "schools":schools}
+
+        except: #학교가 검색되지 않았을때
+            return {"code":416}
+        
+    #api 에러
+    else:
+        return {"code":response.getcode()}
+
+
 
 def getMeal(officeCode, schlCode, date=datetime.datetime.now().strftime("%Y%m%d")):
     url = "https://api.skybro2004.com/meal"
@@ -12,14 +47,14 @@ def getMeal(officeCode, schlCode, date=datetime.datetime.now().strftime("%Y%m%d"
     url += f"&date={date}"
     
     request = ul.Request(url)
-    response = ul.urlopen(url)
+    response = ul.urlopen(request)
 
     if response.getcode()==200:
         responseData = response.read()
         responseData = json.loads(responseData)
 
         if responseData["code"]==200:
-            return {"code":200, "data":responseData["meal"]}
+            return {"code":200, "data":responseData["meal"], "cal":responseData["cal"]}
 
         elif responseData["code"]==404:
             return {"code":416, "data":"급식이 없어요!"}
@@ -30,4 +65,5 @@ def getMeal(officeCode, schlCode, date=datetime.datetime.now().strftime("%Y%m%d"
 
 
 if __name__=="__main__":
+    print(getSchl("서현"))
     print(getMeal("J10", "7530081", "20210302"))
