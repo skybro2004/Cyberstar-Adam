e871e335ca2f5b48c0aef9c6e822164470b088f4
skybro2004 <skybro2004@gmail.com>
2022-03-06 22:06:23 +0900

    주석처리

--- bot.py
+++ bot.py
@@ -15,13 +15,16 @@ path = os.path.dirname(os.path.realpath(__file__)).replace("\\", "/")
 from modules import hangang
 from modules import meal
 
+#데이터베이스 불러오기
 con = sqlite3.connect(f"{path}/data/database.db")
 cursor = con.cursor()
 
+#디스코드 봇 토큰 불러오기
 keys = {}
 try:
     with open(path + "/key.json", "r") as jsonKeys:
         keys = json.load(jsonKeys)
+#토큰이 없다면 입력받아 저장하기
 except FileNotFoundError:
     discordToken = input("디스코드 봇의 토큰을 입력하세요 : ")
     keys = {
@@ -30,13 +33,16 @@ except FileNotFoundError:
     with open(path + "/key.json", "w") as jsonKeys:
         json.dump(keys, jsonKeys)
 
+#봇에게 권한 부여
 intents = discord.Intents.all()
+#시작
 bot = discord.Bot(intents=intents)
 
 
 #==================================================
 
 
+#로그인시 할 행동
 @bot.event
 async def on_ready():
     activity = discord.Activity(type=discord.ActivityType.listening, name="나잠수 - 사이버가수 아담")
@@ -85,6 +91,8 @@ async def meals(
         date: discord.Option(str, name="날짜", description="급식을 불러올 날짜를 선택합니다. YYMMDD 형식으로 입력받습니다.", default=datetime.date.today().strftime("%Y%m%d")),
         mode: discord.Option(str, description="등록: 저장된 사용자 정보를 등록/수정합니다.", choices=["등록"], required=False)
     ):
+
+    #날짜 포매팅
     if date=="오늘":
         date = datetime.date.today().strftime("%Y%m%d")
     elif date=="내일":
@@ -99,8 +107,10 @@ async def meals(
         await ctx.respond("날짜 입력 형식이 잘못되었어요!")
         return
 
+    #사용자 아이디 임시 저장
     author = ctx.author.id
 
+    #저장된 사용자 데이터 중 아이디 불러오기
     cursor.execute("SELECT id FROM userData")
     userIdList = []
     for item in cursor.fetchall():
@@ -108,10 +118,13 @@ async def meals(
 
     #급식 불러오기
     if (author in userIdList) and mode!="등록":
+        #사용자 데이터 불러오기
         cursor.execute(f"SELECT * FROM userData WHERE id={author}")
         userData = cursor.fetchone()
+        #meal 모듈을 통해 급식 정보 불러오기
         todayMeal = meal.getMeal(userData[1], userData[2], date)
 
+        #정상
         if todayMeal["code"]==200:
             mealStr = []
             for item in todayMeal["data"]:
@@ -122,9 +135,11 @@ async def meals(
 
             await ctx.respond(embed=mealEmbed)
 
+        #급식이 없음
         elif todayMeal["code"]==416:
-            await ctx.respond("오늘은 급식이 없습니다.")
+            await ctx.respond("급식이 없습니다.")
 
+        #알 수 없는 에러
         else:
             await ctx.respond(f"알 수 없는 에러가 발생했습니다.\nError no.{todayMeal['code']}")
 
@@ -132,56 +147,77 @@ async def meals(
     else:
         botMsg = await ctx.respond("학교 이름을 입력해주세요")
 
+        #wait_for 대응 함수(메시지 작성자가 등록중인 사용자인가?)
         def check(m):
             return m.author.id == ctx.author.id and m.channel.id == botMsg.channel.id
 
+        #학교 이름 입력받기
         while 1:
+            #메시지 입력 대기
             try:
                 userMsg = await bot.wait_for("message", timeout=30.0, check=check)
+            #타임아웃
             except asyncio.TimeoutError:
                 await botMsg.edit_original_message(content="타임아웃")
                 return
+            #메시지 입력 후
             else:
                 schlName = userMsg.content
                 await userMsg.delete()
 
                 schlData = meal.getSchl(schlName)
 
+                #학교 검색 성공
                 if schlData["code"]==200:
                     break
 
+                #학교 검색 결과 없으면 다시 입력
                 elif schlData["code"]==416:
                     await botMsg.edit_original_message(content="학교가 검색되지 않았어요.\n다시 입력해주세요")
+                    continue
 
+                #에러
                 else:
                     await botMsg.edit_original_message(content=f"알 수 없는 에러가 발생했어요.\nError no.{schlData['code']}")
                     return
 
+        #학교 목록 불러오기
         schlData = schlData["schools"]
         schools = []
         for school in schlData:
             schools.append(discord.SelectOption(label=school["schlName"], description=school["office"], value=json.dumps({"schlCode":school["schlCode"], "officeCode":school["officeCode"]})))
 
+        #사용자 선택 저장용 글로벌 변수
         global selection
         selection = ""
 
+        #컴포넌트 클래스
         class selectSchl(discord.ui.View):
+            #학교 목록 컴포넌트
             @discord.ui.select(placeholder="학교를 선택하세요", options=schools)
             async def select(self, select: discord.ui.Select, interaction: discord.Interaction):
+                #선택
+                #인터랙션한 사용자==등록중인 사용자인가
                 if author==interaction.user.id:
+                    #아까 만든 변수에 선택한 값 저장
                     global selection
                     selection = json.loads(interaction.data["values"][0])
 
-
+            #확인 버튼 컴포넌트
             @discord.ui.button(label="확인", ButtonStyle=discord.ButtonStyle.green)
             async def accept(self, button: discord.ui.Button, interaction: discord.Interaction):
+                #선택 완료
+                #인터랙션한 사용자==등록중인 사용자인가
                 if author==interaction.user.id:
+                    #아까 만든 변수 불러오기
                     global selection
+                    #아무것도 선택 안했을때
                     if selection=="":
                         await ctx.respond("학교를 선택해주세요!", ephemeral=True)
+
                     else:
                         try:
-                            #DB에 저장
+                            #선택한 학교 DB에 저장
                             cursor.execute(
                                 "INSERT INTO userData(id, officeCode, schlCode) VALUES(?, ?, ?)",
                                 (author, selection["officeCode"], selection["schlCode"])
@@ -194,10 +230,13 @@ async def meals(
                                 (selection["officeCode"], selection["schlCode"], author)
                             )
 
+                        #DB 적용
                         con.commit()
 
+                        #설정 완료 얼럿(5초후 자동 삭제)
                         await botMsg.edit_original_message(content="설정 완료!", view=None, delete_after=5.0)
 
+        #학교 검색결과 컴포넌트와 함께 표시
         await botMsg.edit_original_message(content=f"\"{schlName}\" 검색결과", view=selectSchl())
 
 
@@ -210,6 +249,8 @@ async def schedular(ctx):
 #/한강
 @bot.slash_command(name="한강", guild_ids = [803249696638238750], description="자살 하면 그만이야~")
 async def getHangang(ctx):
+    """한강 api 딜레이 심해서 디코봇이 응답안함으로 인식함. 그래서 일단 메시지 보내고 수정하는 방식 채용"""
+
     respondMsg = await ctx.respond(f"한강 수온을 불러오는중...")
     await respondMsg.edit_original_message(content = f"현재 한강 수온 : {hangang.getTemp()}°C")
 
@@ -247,4 +288,6 @@ async def hidden(ctx):
     await ctx.respond("awsdf", view=components)
 
 
+
+#봇 실행
 bot.run(keys["discordToken"])
